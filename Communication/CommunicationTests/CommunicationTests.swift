c42baddbdd35a0918156635bd9038e09cd34ca79
Ryley Herrington <ryley.herrington@nike.com>
2014-10-06 10:23:05 -0700

    Initial commit for ios

---
+++ Communication/CommunicationTests/CommunicationTests.swift
@@ -0,0 +1,36 @@
+//
+//  CommunicationTests.swift
+//  CommunicationTests
+//
+//  Created by Herrington, Ryley on 10/6/14.
+//  Copyright (c) 2014 Brogrammers. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class CommunicationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
